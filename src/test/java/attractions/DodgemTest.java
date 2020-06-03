17982895b9ef80251e50f7f0fb4ebb4f614083d9
CrugBarat <craigtbarratt@gmail.com>
2020-06-03 01:50:10 +0100

    implements ITicketed interface into Dodgem class with corresponding methods and tests - PASSED
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- src/test/java/attractions/DodgemTest.java
+++ src/test/java/attractions/DodgemTest.java
@@ -2,16 +2,20 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class DodgemTest {
 
     Dodgems dodgems;
+    Visitor visitor1, visitor2;
 
     @Before
     public void setUp() throws Exception {
         dodgems = new Dodgems("Bumper Cars", 5);
+        visitor1 = new Visitor(11, 1.2, 40.0);
+        visitor2 = new Visitor(16, 1.5, 20.0);
     }
 
 
@@ -28,5 +32,20 @@ public class DodgemTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, dodgems.getVisitCount());
+    }
+
+    @Test
+    public void canGetDefaultPrice() {
+        assertEquals(4.50, dodgems.defaultPrice(), 0.01);
+    }
+
+    @Test
+    public void canGetPriceForVisitorUnder12() {
+        assertEquals(2.25, dodgems.priceFor(visitor1), 0.01);
+    }
+
+    @Test
+    public void canGetPriceForVisitorOver12() {
+        assertEquals(4.50, dodgems.priceFor(visitor2), 0.01);
     }
 }
