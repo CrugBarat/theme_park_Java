19fb1d02ebcc87d623a7c184c9b2db2d94b50b4f
CrugBarat <craigtbarratt@gmail.com>
2020-06-03 01:12:01 +0100

    implements ISecurity into Playground class with isAllowedTo method and tests - PASSED
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- src/test/java/attractions/PlaygroundTest.java
+++ src/test/java/attractions/PlaygroundTest.java
@@ -2,15 +2,20 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class PlaygroundTest {
+
     Playground playground;
+    Visitor visitor1, visitor2;
 
     @Before
     public void setUp() throws Exception {
         playground = new Playground("Fun Zone", 7);
+        visitor1 = new Visitor(14, 1.2, 40.0);
+        visitor2 = new Visitor(16, 1.5, 20.0);
     }
 
     @Test
@@ -26,5 +31,15 @@ public class PlaygroundTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, playground.getVisitCount());
+    }
+
+    @Test
+    public void canCheckForAgeOver15() {
+        assertEquals(false, playground.isAllowedTo(visitor2));
+    }
+
+    @Test
+    public void canCheckForAgeUnder15() {
+        assertEquals(true, playground.isAllowedTo(visitor1));
     }
 }
