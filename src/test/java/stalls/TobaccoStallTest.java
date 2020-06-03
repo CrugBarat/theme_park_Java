9465b21c29ad3df14a068d576a65df7cd050008e
CrugBarat <craigtbarratt@gmail.com>
2020-06-03 01:15:17 +0100

    implements ISecurity into TobaccoStall class with isAllowedTo method and tests - PASSED
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- src/test/java/stalls/TobaccoStallTest.java
+++ src/test/java/stalls/TobaccoStallTest.java
@@ -2,16 +2,20 @@ package stalls;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class TobaccoStallTest {
 
     TobaccoStall tobaccoStall;
+    Visitor visitor1, visitor2;
 
     @Before
     public void setUp() throws Exception {
         tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1);
+        visitor1 = new Visitor(19, 1.2, 40.0);
+        visitor2 = new Visitor(16, 1.5, 20.0);
     }
 
     @Test
@@ -27,5 +31,15 @@ public class TobaccoStallTest {
     @Test
     public void hasParkingSpot() {
         assertEquals(ParkingSpot.B1, tobaccoStall.getParkingSpot());
+    }
+
+    @Test
+    public void canCheckForAgeOver18() {
+        assertEquals(true, tobaccoStall.isAllowedTo(visitor1));
+    }
+
+    @Test
+    public void canCheckForAgeUnder18() {
+        assertEquals(false, tobaccoStall.isAllowedTo(visitor2));
     }
 }
