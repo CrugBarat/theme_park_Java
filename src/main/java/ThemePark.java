5a2125e9df50ca23852f9f34f41bced35aa5a611
CrugBarat <craigtbarratt@gmail.com>
2020-06-03 02:40:39 +0100

    adds review list and associated methods with tests - PASSED
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- src/main/java/ThemePark.java
+++ src/main/java/ThemePark.java
@@ -1,13 +1,16 @@
 import attractions.Attraction;
+import behaviours.IReviewed;
 
 import java.util.ArrayList;
 
 public class ThemePark {
 
     private ArrayList<Attraction> attractions;
+    private ArrayList<IReviewed> reviews;
 
     public ThemePark() {
         this.attractions = new ArrayList<>();
+        this.reviews = new ArrayList<>();
     }
 
     public int getAttractionsSize() {
@@ -18,4 +21,15 @@ public class ThemePark {
         this.attractions.add(attraction);
     }
 
+    public int getReviewListSize() {
+        return this.reviews.size();
+    }
+
+    public void addReview(IReviewed review) {
+        this.reviews.add(review);
+    }
+
+    public ArrayList<IReviewed> getAllReviewed() {
+        return this.reviews;
+    }
 }
--- src/test/java/ThemeParkTest.java
+++ src/test/java/ThemeParkTest.java
@@ -1,19 +1,22 @@
 import attractions.Attraction;
 import attractions.Dodgems;
+import behaviours.IReviewed;
 import org.junit.Before;
 import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
+
 
 public class ThemeParkTest {
 
     ThemePark themePark;
     Attraction attraction1;
+    IReviewed review;
 
     @Before
     public void before() {
         themePark = new ThemePark();
         attraction1 = new Dodgems("Bumper Cars", 5);
+        review = new Dodgems("Bumper Cars", 5);
     }
 
     @Test
@@ -26,4 +29,16 @@ public class ThemeParkTest {
         themePark.addAttraction(attraction1);
         assertEquals(1, themePark.getAttractionsSize());
     }
+
+    @Test
+    public void reviewSizeStartsAtZero() {
+        assertEquals(0, themePark.getReviewListSize());
+    }
+
+    @Test
+    public void canAddReview() {
+        themePark.addReview(review);
+        assertEquals(1, themePark.getReviewListSize());
+    }
+
 }
